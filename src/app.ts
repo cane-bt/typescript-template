0e4597c1c476f6a44a0a9a9816882aa7dc690415
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-12 16:50:19 +0900

    62.privat修飾子とpublic修飾子

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,9 @@
-// class Department {
-//     name: string;
-//     // 初期値も設定できる
-//     // name: string = 'Default'
-
-//     // constructorメソッド
-//     //  予約語。
-//     constructor(n: string) {
-//         this.name = n;
-//     }
-
-//     describe() {
-//         // 以下の書き方だとクラスの外のグローバル変数を参照するため正しく動作しない
-//         // console.log('Department' + name);
-//         // クラスのプロパティを参照するにはthisを使用する
-//         // ここでのthisはクラスから作成された具体的なプロパティを参照する
-//         console.log('Department: ' + this.name);
-
-//     }
-// }
-
-// const accounting = new Department('Accounting');
-
-// accounting.describe()
-
-// // describeメソッドをオブジェクトに対して追加しているため期待した動作にならない
-// //  オプジェクトリテラルで作られたオブジェクトでありクラスに基づいて作成されたオブジェクトではない
-// //      クラスを使わずに単なるダミーオブジェクトとして作られたもの
-// //      describeプロパティの値はaccountingオブジェクトのdescribeメソッドを指している
-// //      実行はしていないので関数の実行結果の値をdescribeに渡しているのではない
-// //  Department: undefined と表示される
-// const accountingCopy = { describe: accounting.describe }
-// // ここではaccountigCopyのdescribeメソッドを参照しているが、thisプロパティが存在しないため undefined になる
-// accountingCopy.describe()
-
-
+// private & public 修飾子
 class Department {
     name: string;
-    // 初期値も設定できる
-    // name: string = 'Default'
+    // privateをつけることで外部からアクセスできないようにできる
+    private employees: string[] = [];
 
-    // constructorメソッド
-    //  予約語。
     constructor(n: string) {
         this.name = n;
     }
@@ -48,17 +11,27 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+// 外部(クラスの外部も含む)から直接変更できてしまう
+// accounting.employees[2] = 'Anna'
+
 accounting.describe()
+accounting.printEmployeeInformation();
 
-// describeにthisを追加することでエラーを検知してくれる
-// const accountingCopy = { describe: accounting.describe }
+// const accountingCopy = { name: 'dummy', describe: accounting.describe }
 // accountingCopy.describe()
-
-// 以下は正しく動作する
-//  Departmentと同じようにnameプロパティを持っていて全く同じ構造のため
-const accountingCopy = { name: 'dummy', describe: accounting.describe }
-accountingCopy.describe()
