0b1865db0261b7e603f8eb664707654bef85c4d6
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-14 14:54:32 +0900

    環境構築を修正、動作確認完了

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+// https://typescriptbook.jp/tutorials/prettier#%E8%A8%AD%E5%AE%9A%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92%E4%BD%9C%E6%88%90%E3%81%99%E3%82%8B
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
