ff1de12c4dabefe9be5f679b5e5fda72357eb18b
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-17 14:45:04 +0900

    76. インターフェースの拡張

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
+// 76. インターフェースの拡張
+interface Named {
+  readonly name: string;
+}
 
+// interfaceに別のinterfaceを継承する
+//  interfaceは複数のinterfaceを継承できる
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
+  greet(phrase: string): void {}
+}
