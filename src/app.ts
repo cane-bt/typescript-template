1ef7cb83cd81a3721bc6d0904a7e0b1b298a2d95
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-19 14:18:31 +0900

    84. 型ガード

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// 84. 型ガード
 type Admin = {
   name: string;
   priviledges: string[];
@@ -20,3 +21,62 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+  // オブジェクトにプロパティがあるか確認する
+  if ('privileges' in emp) {
+    console.log('Priviledges: ' + emp.privileges);
+  }
+  if ('startDate' in emp) {
+    console.log('StartDate: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log('運転中...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('トラックを運転中...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('荷物を載せています...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //   if ('loadCargo' in vehicle) {
+  //     vehicle.loadCargo(1000);
+  //   }
+
+  // vehicleがtruckのクラスであるか確認
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
