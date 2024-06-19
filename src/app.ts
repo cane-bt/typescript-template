0ddcb94622c3121fd28ae1a46dd6dd2960052739
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-19 09:27:11 +0900

    83. 交差型

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+// 83. 交差型
+
+type Admin = {
+  name: string;
+  priviledges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  priviledges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
