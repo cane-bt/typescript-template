0ecc7291fc340216e93ec197dbe2f7247990bbfc
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-20 11:51:07 +0900

    初期化

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
-// 86. 型キャスト
-//  TypeScriptでもokがある特定の型であるということをはっきりと明確に推論できない使用する
-//  開発者が知っているその特定の型をTypeScriptに伝えることができる
 interface Bird {
   type: 'bird';
   flyingSpeed: number;
@@ -25,23 +22,3 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
-
-// ! を使わない書き方
-// const userInputElement = document.getElementById('user-input');
-
-// if (userInputElement) {
-//   (userInputElement as HTMLInputElement).value = 'こんにちは';
-// }
-
-// ! を使う書き方
-// const paragraph = document.querySelector('message-output');
-// const userInputElement = <HTMLInputElement>(
-//   document.getElementById('user-input')!
-// );
-// userInputElement.value = 'こんにちは';
-
-// react, jsxで見る書き方(TypeScriptでもok)
-const userInputElement = document.getElementById(
-  'user-input'
-)! as HTMLInputElement;
-userInputElement.value = 'こんにちは';
