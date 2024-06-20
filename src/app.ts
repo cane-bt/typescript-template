30d928c3cdedf01787de3a1839895eab12b2a11d
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-20 14:14:46 +0900

    初期化

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,29 @@
-// 87. インデックス型
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-  }
-  console.log('移動速度: ' + speed);
-}
-
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
-
-const userInputElement = document.getElementById('user-input');
-
-if (userInputElement) {
-  (userInputElement as HTMLInputElement).value = 'こんにちは';
-}
-
-interface ErrorContainer {
-  // {email:'正しいメールアドレスではありません', username:'ユーザー名に記号を含めることはできません'}
-  [prop: string]: string;
-}
-
-const errorBag: ErrorContainer = {
-  email: '正しいメールアドレスではありません',
-  username: 'ユーザー名に記号を含めることはあできません',
+type Admin = {
+  name: string;
+  priviledges: string[];
 };
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  priviledges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
