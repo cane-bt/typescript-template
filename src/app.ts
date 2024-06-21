20b9e0cfdc1c47ff37933ddb9ca1343096601242
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-21 14:44:23 +0900

    98. もうひとつの Generic 関数

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-// 97. Generics に制約を追加する
-// object型の型制限を追加して30に対してエラー表示させるようにする
+// 98. もうひとつの Generic 関数
 function merge<T extends {}, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, 30);
+const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 
 console.log('mergedObject: ', mergedObject);
+
+// 型があいまいのため`length`でエラーが表示される
+// function countAndDescribe<T>(element: T) {
+//   let descriptionText = '値がありません';
+//   if (element.length > 0) {
+//     descriptionText = '値は' + element.length + '個です';
+//   }
+//   return [element, descriptionText];
+// }
+
+interface Lengthy {
+  length: number;
+}
+// 型があいまいのため`length`でエラーが表示される
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = '値がありません';
+  if (element.length > 0) {
+    descriptionText = '値は' + element.length + '個です';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('お疲れ様です'));
