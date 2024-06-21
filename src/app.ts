2e07bc6284fc0f1b2c6e9624284d7b7453c317e4
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-21 13:18:42 +0900

    初期化

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-// 94. 組み込みの Generic 型 & Generics とは
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('');
-  }, 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('');
+//   }, 2000);
+// });
 
-promise.then((data) => {data.split(' '));
+// promise.then((data) => {data.split(' '));
