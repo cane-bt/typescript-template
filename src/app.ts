d97419b5a03029d8b64df5fe778a100da6a21023
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-21 14:11:54 +0900

    97. Generics に制約を追加する

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-function merge<T extends {}, U>(objA: T, objB: U) {
+// 97. Generics に制約を追加する
+// object型の型制限を追加して30に対してエラー表示させるようにする
+function merge<T extends {}, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, 30);
 
 console.log('mergedObject: ', mergedObject);
