2a1a1339d3cea009288e4820958fd6674d218a2c
toshi-ue <t.ueno0207+github@gmail.com>
2024-06-24 07:59:04 +0900

    generic

--- src/app.ts
+++ src/app.ts
@@ -49,3 +49,66 @@ function extractAndConvert<T extends object, U extends keyof T>(
 extractAndConvert({ name: 'Max' }, 'name');
 // 期待通りエラーが検知される
 // extractAndConvert({}, 'name');
+
+// Genericクラス
+// class DataStorage<T> {
+//   private data: T[] = [];
+
+//   addItem(item: T) {
+//     this.data.push(item);
+//   }
+
+//   removeItem(item: T) {
+//     this.data.splice(this.data.indexOf(item), 1);
+//   }
+
+//   getItems() {
+//     return [...this.data];
+//   }
+// }
+
+// const textStorage = new DataStorage<string>();
+// textStorage.addItem('Data1');
+// textStorage.addItem('Data2');
+// textStorage.removeItem('Data1');
+// console.log(textStorage.getItems());
+
+// const numberStorage = new DataStorage<number>();
+// numberStorage.addItem(1);
+// numberStorage.addItem(2);
+// numberStorage.removeItem(1);
+// console.log(numberStorage.getItems());
+
+// プリミティブ型は問題ないがオブジェクト型だと問題あり
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({ name: 'Max' });
+// objectStorage.addItem({ name: 'Manu' });
+// objectStorage.removeItem({ name: 'Max' });
+// // Maxを削除しているのにMaxが表示される
+// console.log(objectStorage.getItems());
+
+// オブジェクト型を許可させないようにクラスを定義する(期待通りの動作)
+//   extendsでプリミティブ型のみ許可するように制限する
+class DataStorage2<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+// オブジェクト型が許可されなくなる
+const objectStorage = new DataStorage2<object>();
+objectStorage.addItem({ name: 'Max' });
+objectStorage.addItem({ name: 'Manu' });
+objectStorage.removeItem({ name: 'Max' });
+// Maxを削除しているのにMaxが表示される
+console.log(objectStorage.getItems());
